edef867b9a019ec4229c71a66e3374cb61011274
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-30 22:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 //your JS code here. If required.
 // Select all input elements with class "code"
-const codeInputs = document.querySelectorAll(".code");
-
-codeInputs.forEach((input, index) => {
-    input.addEventListener("input", (e) => {
-        // Move focus to the next input if there is a value and it's not the last input
-        if (e.target.value && index < codeInputs.length - 1) {
-            codeInputs[index + 1].focus();
-        }
-    });
-
-    input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace") {
-            // Clear current input on backspace
-            e.target.value = "";
-
-            // Move focus to the previous input if it exists
-            if (index > 0) {
-                codeInputs[index - 1].focus();
-            }
-        }
-    });
-});
